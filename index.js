a809e3065a24e168285b21f514190984d143b652
Yeison Cano <yeisonc2002@gmail.com>
2023-04-15 01:45:48 -0500

    commit Solucion error de alerta

--- index.js
+++ index.js
@@ -2,26 +2,23 @@ function encriptar() {
     let texto = document.getElementById("texto").value;
     let tituloMensaje = document.getElementById("titulo-mensaje");
     let parrafo = document.getElementById("parrafo");
-    let muñeco= document.getElementById("imagen-muñeco")
-
+    let muñeco = document.getElementById("imagen-muñeco");
 
     let textoCifrado = texto
-        .repleace(/a/gi, "ai")
-        .repleace(/e/gi, "enter")
-        .repleace(/i/gi, "imes")
-        .repleace(/o/gi, "ober")
-        .repleace(/u/gi, "ufat");
+        .replace(/a/gi, "ai")
+        .replace(/e/gi, "enter")
+        .replace(/i/gi, "imes")
+        .replace(/o/gi, "ober")
+        .replace(/u/gi, "ufat");
 
     if (texto.length != 0) {
-        texto = textoCifrado;
+        document.getElementById("texto").value = textoCifrado;
         tituloMensaje.textContent = "Texto encriptado con exito.";
         parrafo.textContent = "";
-        muñeco.src="imagenes/online-store.png"; 
+        muñeco.src ="./imagenes/online-store.png"; 
     }
     else {
-        muñeco.src="imagenes/Muñeco.png";
         alert("Debes ingresar algun texto.");
     }
-
 }
 
